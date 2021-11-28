dbfc698f6b70f952e1674aa70e71c4705d79d519
Harish Ramachandra <anamethatistaken@gmail.com>
2021-11-28 10:51:18 +0000

    new update 2

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -10,12 +10,15 @@ import EntryModal from '$lib/EntryModal.svelte';
 <section class="container px-4 py-3">
     <div class="d-flex justify-content-between">
    	 <div class="p-2">Mood Log</div>
-   	 <input class="btn btn-light mb-2" type="button" value="+ New Entry" />
+   	 <input class="btn btn-light mb-2" 
+        data-bs-toggle="modal" data-bs-target="#newEntry"
+        type="button" value="+ New Entry" />
     </div>
 
     <div class="list-group mb-3">
    	 <!-- Individual Entries -->
     	<Entry />
-   	 
+   	   
+
     </div>
 </section>
